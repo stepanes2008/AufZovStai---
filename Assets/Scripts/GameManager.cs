e34a7e18099d46c436f10b4e27c3a653d0a0a4c1
stepanes2008 <akm2008@mail.ru>
2024-04-30 23:08:30 +0300

    MenuCompleted
    Теперь можно зайти в игру и поставить ее на паузу через UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Bye!");
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameplayScene");
+    }
+}
+
---
+++ Assets/Scripts/PauseOnESC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseOnESC : MonoBehaviour
+{
+    public GameObject pauseScreen;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            /*SceneManager.LoadScene("MenuScene");*/
+            pauseScreen.SetActive(true);
+        }
+    }
+    public void ContinueGame()
+    {
+        pauseScreen.SetActive(false);
+    }
+    public void ExitToMainMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
