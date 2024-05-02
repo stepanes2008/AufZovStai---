d9c62efb97cb29d2be3b8b9949024dacb3dfeb9a
stepanes2008 <akm2008@mail.ru>
2024-05-02 22:27:18 +0300

    AdvancedMenu
    Начал с менюшками для смены уровня

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,9 @@ public class GameManager : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
-        Debug.Log("Bye!");
     }
     public void StartGame()
     {
         SceneManager.LoadScene("GameplayScene");
     }
 }
-
---
+++ Assets/Scripts/MenuScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScreen : MonoBehaviour
+{
+    private List<MenuScreen> _otherScreens;
+
+    private void Start()
+    {
+        _otherScreens = new List<MenuScreen>(transform.parent.GetComponentsInChildren<MenuScreen>());
+        var index = _otherScreens.IndexOf(this);
+        _otherScreens.Remove(this);
+
+        if (index > 0) return;
+        Show();
+    }
+
+    public void Show()
+    {
+        foreach(var screen in _otherScreens)
+        {
+            screen.gameObject.SetActive(false);
+        }
+        gameObject.SetActive(true);
+    }
+}
