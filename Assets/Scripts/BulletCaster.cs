1fa00c0284cf51c785f03fd32136a2a1448c3a9d
stepanes2008 <akm2008@mail.ru>
2024-05-02 12:21:34 +0300

    Camera+Bullet
    Вращение камеры вокруг перса + стрельба очередью

---
+++ Assets/Scripts/BulletCaster.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCaster : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float damage = 5f;
+
+    public AudioClip shootSound;
+    public GameObject Player;
+    public GameObject Bullet;
+    public GameObject SpawnPoint;
+    private float _reloadDelay = 0f;
+    private bool _shootingActivated = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BulletUpdate();
+    }
+    private void BulletUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _shootingActivated = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            _shootingActivated = false;
+        }
+        if (_shootingActivated)
+        {
+            float spawnDelay = Random.Range(0.08f, 0.2f);
+            Invoke("SpawnBullet", spawnDelay);
+        }
+    }
+    private void SpawnBullet()
+    {
+        /*            Player.GetComponent<Animator>().SetTrigger("Draw");
+                                Player.GetComponent<Animator>().SetTrigger("Shoot");
+                                audioSource.PlayOneShot(shootSound);*/
+        _reloadDelay = 0f;
+        var Arr = Instantiate(Bullet, SpawnPoint.transform.position, SpawnPoint.transform.rotation);
+        //            Arr.transform.position = SpawnPoint.transform.position;
+        Arr.transform.eulerAngles = new Vector3(SpawnPoint.transform.eulerAngles.x + 90f, SpawnPoint.transform.eulerAngles.y, SpawnPoint.transform.eulerAngles.z);
+        Arr.GetComponent<BulletController>().damage = damage;
+    }
+}
---
+++ Assets/Scripts/BulletSource.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSource : MonoBehaviour
+{
+    public Transform targetPoint;
+    public Camera cameraLink;
+    public float targetInSkyDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = cameraLink.ViewportPointToRay(new Vector3(0.5f, 0.7f, 0f));
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetPoint.position = hit.point;
+        }
+        else
+        {
+            targetPoint.position = ray.GetPoint(targetInSkyDistance);
+        }
+        transform.LookAt(targetPoint.position);
+    }
+}
+
