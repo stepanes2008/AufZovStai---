8bb79c61511d79722b9cabf9f3ef7ba224e72215
stepanes2008 <akm2008@mail.ru>
2024-05-05 16:03:41 +0300

    Mobs Moving
    Теперь враг (пока прямоугольник) может двигаться по городу

---
+++ Assets/Scripts/WolfAI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WolfAI : MonoBehaviour
+{
+    public List<Transform> patrolPoints;
+
+    private NavMeshAgent _navMeshAgent;
+    private Vector3 LastPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PatrolUpdate();
+    }
+
+    private void PatrolUpdate()
+    {
+        if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+        {
+            PickNewPatrolPoint();
+        }
+    }
+
+    private void PickNewPatrolPoint()
+    {
+        LastPosition = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+}
